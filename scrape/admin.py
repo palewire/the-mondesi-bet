2da0c40a3cf9362a36d9a1ed0ca61ec2b341534e
palewire <ben.welsh@gmail.com>
2019-03-29 06:53:04 -0700

    GameLog admin

--- scrape/admin.py
+++ scrape/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from scrape.models import SeasonTotal
+from scrape.models import SeasonTotal, GameLog
 
 
 @admin.register(SeasonTotal)
 class SeasonTotalAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(GameLog)
+class GameLogAdmin(admin.ModelAdmin):
     pass
