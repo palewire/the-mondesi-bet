2a972d0648806f70faede29fef61c2b9b4077894
palewire <ben.welsh@gmail.com>
2019-04-07 20:35:04 -0700

    Other projections

--- scrape/management/commands/scrapeprojections.py
+++ scrape/management/commands/scrapeprojections.py
@@ -14,14 +14,43 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         data = self.get_data()
         dumped = json.dumps(data, indent=4)
-        obj = Projection.objects.create(json=dumped)
-        print(f"Created {obj}")
+        print(dumped)
+        # obj = Projection.objects.create(json=dumped)
+        # print(f"Created {obj}")
 
     def get_data(self):
         return {
             "steamer-update": self.get_steamer_update(),
-            "thebat-ros": self.get_thebat_ros()
+            "thebat-ros": self.get_thebat_ros(),
+            "zips-update": self.get_zips_update(),
+            'depthcharts-ros': self.get_depthcharts_ros()
         }
+
+    def get_depthcharts_ros(self):
+        url = "https://www.fangraphs.com/projections.aspx?pos=all&stats=bat&type=rfangraphsdc&team=7&lg=all&players=0"
+        r = requests.get(url)
+        soup = BeautifulSoup(r.text, "html5lib")
+        table = soup.find('table', id="ProjectionBoard1_dg1_ctl00")
+        for row in table.tbody.find_all("tr"):
+            cell_list = row.find_all("td")
+            if cell_list[0].text == "Adalberto Mondesi":
+                return {
+                    "hr": int(cell_list[9].string),
+                    "sb": int(cell_list[15].string)
+                }
+
+    def get_zips_update(self):
+        url = "https://www.fangraphs.com/projections.aspx?pos=all&stats=bat&type=uzips&team=7&lg=all&players=0"
+        r = requests.get(url)
+        soup = BeautifulSoup(r.text, "html5lib")
+        table = soup.find('table', id="ProjectionBoard1_dg1_ctl00")
+        for row in table.tbody.find_all("tr"):
+            cell_list = row.find_all("td")
+            if cell_list[0].text == "Adalberto Mondesi":
+                return {
+                    "hr": int(cell_list[9].string),
+                    "sb": int(cell_list[15].string)
+                }
 
     def get_steamer_update(self):
         url = "https://www.fangraphs.com/projections.aspx?pos=all&stats=bat&type=steameru&team=7&lg=all&players=0"
